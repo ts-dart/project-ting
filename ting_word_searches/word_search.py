fd66cfca2dd8b4564914dde4ae213ee1301023e6
thiago <thiagoedusan5.11@outlook.com>
2022-12-06 19:46:57 -0300

    feat: foi desenvolvida a função exists_word no modulo file_process

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,19 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    arr = list()
+
+    for file in instance.__str__():
+        arr.append({
+            'palavra': word,
+            'arquivo': file['nome_do_arquivo'],
+            'ocorrencias': []
+        })
+
+        lines = file['linhas_do_arquivo']
+        for n in range(len(lines)):
+            if word.lower() in lines[n].lower():
+                arr[0]['ocorrencias'].append({'linha': n+1})
+
+    return arr if len(arr[0]['ocorrencias']) > 0 else []
 
 
 def search_by_word(word, instance):
