80caaad203076533f311982c5fcd908b9a35e892
thiago <thiagoedusan5.11@outlook.com>
2022-12-06 14:20:48 -0300

    feat: foi desenvolvida a função process no modulo file_process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,25 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    list = txt_importer(path_file)
+    repeated = False
+
+    for data in instance.__str__():
+        if data['nome_do_arquivo'] == path_file:
+            repeated = True
+
+    if not repeated:
+        dict = {
+            'nome_do_arquivo': path_file,
+            'qtd_linhas': len(list),
+            'linhas_do_arquivo': list
+        }
+
+        instance.enqueue(dict)
+        print(str(dict))
+        sys.stderr.write(str(dict))
 
 
 def remove(instance):
